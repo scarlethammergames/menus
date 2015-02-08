7b92b4b5075efd9fca81198ed3f628eec4df3407
Monica Ung <monicaung3@gmail.com>
2015-02-08 12:10:41 -0500

    Added two main menus
    MainMenu is a simple menu with Start and Quit buttons.  MainMenu2 has
    sliding animation and has an additional settings menu that slides in
    when you click on Settings button.

---
+++ UI/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	public GUISkin gSkin;
+
+	void Start() {
+		float fontSize = Screen.height * 0.04f;
+		gSkin.button.fontSize = (int) fontSize;
+	}
+	void OnGUI() {
+		GUI.skin = gSkin;
+
+		if (GUI.Button(new Rect(Screen.width*0.5f - (Screen.width*0.2f/2), Screen.height*0.4f,Screen.width*0.2f,Screen.height*0.1f), "Play")) {
+			Application.LoadLevel("Game");
+		}
+
+		if (GUI.Button(new Rect(Screen.width*0.5f - (Screen.width*0.2f/2), Screen.height*0.6f,Screen.width*0.2f,Screen.height*0.1f), "Quit")) {
+			Application.Quit();
+		}
+	}
+}
---
+++ UI/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuManager : MonoBehaviour {
+
+	public MainMenu2 CurrentMenu;
+
+	public void Start() {
+		ShowMenu (CurrentMenu);
+	}
+
+	public void ShowMenu(MainMenu2 menu) {
+		if (CurrentMenu != null) {
+			CurrentMenu.IsOpen = false;
+		}
+		CurrentMenu = menu;
+		CurrentMenu.IsOpen = true;
+	}
+	public void PlayGame(string sceneName) {
+		Application.LoadLevel (sceneName);
+	}
+	public void QuitGame() {
+		Application.Quit();
+	}
+}
